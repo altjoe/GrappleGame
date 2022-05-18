f0f56d2a0f914da69ec3097f8fb2b04ab51d3aa8
JoeMac <meyer.joer5@gmail.com>
2022-05-18 14:31:02 -0400

    not working rope thinking to branch and start with a rope class

--- GrappleGame/Assets/Scripts/PlayerManager.cs
+++ GrappleGame/Assets/Scripts/PlayerManager.cs
@@ -5,142 +5,143 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
 
-    public GameObject player_prefab;
+    public GameObject ball_prefab;
     public GameObject rope_prefab;
     public GameObject hook_prefab;
 
-    public Player player;
-    public Information info;
-
+    Information info;
+    Player player;
+    
     void Start() {
-        info = new Information(this.transform.localPosition, player_prefab, rope_prefab, hook_prefab);
+        info = new Information(ball_prefab, rope_prefab, hook_prefab, this.transform.localPosition);
         player = new Player(info);
     }
 
     void Update() {
-        player.move();
+        if (player.active_hook == false) {
+            Vector2 shoot_dir = new Vector2(1, 1);
+            player.shoot_hook(shoot_dir.normalized);
+        }
     }
 
     void FixedUpdate() {
-        // player.move();
+        player.move();
     }
 
     public class Player {
-        public Vector2 prev_pt;
-        public Vector2 curr_pt;
+        Information info;
+        List<Rigidbody2D> ball_rope_hook_rb = new List<Rigidbody2D>();
+        List<Vector2> ball_rope_hook_prev_pos = new List<Vector2>(); 
+        Rigidbody2D hook_obj;
+        Vector2 prev_hook_pos;
+        float shoot_speed = 1;
+        public bool active_hook = false;
+        public bool hook_hit = false;
+        Vector2 shoot_drection;
 
-        public GameObject player_obj;
-        public Rigidbody2D player_rb;
+        public Player(Information i) {
+            info = i;
+            GameObject ball_obj = Instantiate(info.ball_prefab, i.start_loc, Quaternion.identity);
+            Rigidbody2D ball_rb = ball_obj.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+            ball_rope_hook_rb.Add(ball_rb);
+            ball_rope_hook_prev_pos.Add(ball_rb.position);
 
-        public Hook hook;
-        public Vector2 prev_rope_point; 
-
-        public Information info;
-        int current_rope_length = 0;
-        float last_seg_dist;
-
-        public Player(Information info){
-            this.player_obj = Instantiate(info.player_prefab, info.start_pt, Quaternion.identity);
-            this.player_rb = this.player_obj.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
-            this.info = info;
-            curr_pt = info.start_pt;
-
-            shoot_hook(Vector2.up);
+            ball_add_force(new Vector2(0.025f, 0.025f));
         }
 
-        public void shoot_hook(Vector2 direction){
-            hook = new Hook(curr_pt, direction, info);
-        }
-
-        public void rope_creation(Vector2 new_hook_loc, Vector2 prev_rope_loc){
-            if ((new_hook_loc - prev_rope_loc).magnitude > info.rope_dist) {
+        public void move() {
+            for (int i = 0; i < ball_rope_hook_rb.Count; i++) {
+                Vector2 diff = ball_rope_hook_rb[i].position - ball_rope_hook_prev_pos[i];
+                ball_rope_hook_prev_pos[i] = ball_rope_hook_rb[i].position;
+                if (i == ball_rope_hook_rb.Count - 1) {
+                    RaycastHit2D hit = Physics2D.Raycast(ball_rope_hook_rb[i].position, ball_rope_hook_prev_pos[i], diff.magnitude, LayerMask.GetMask("Collision"));
+                    if (hit.collider != null) {
+                        ball_rope_hook_rb[i].MovePosition(hit.point);
+                        create_rope();
+                        hook_hit = true;
+                    } 
+                    else {
+                        ball_rope_hook_rb[i].MovePosition(ball_rope_hook_prev_pos[i] + diff + info.gravity);
+                        create_rope();
+                    }
+                    
+                } else {
+                    ball_rope_hook_rb[i].MovePosition(ball_rope_hook_prev_pos[i] + diff + info.gravity);
+                }
                 
             }
-        }
-
-        public void remove_rope() {
-
-        }
-
-        public void add_force(Vector2 force) {
-            prev_pt -= force;
-        }
-
-        public void move(){
-            Vector2 diff = curr_pt - prev_pt;
-            prev_pt = curr_pt;
-            curr_pt += diff;
-            curr_pt += info.gravity;
-            player_rb.MovePosition(curr_pt);
-
-            hook.move();
-            prev_rope_point = rope_creation(curr_pt, prev_rope_point)
-        }
-
-        public class Hook {
-            Vector2 curr_pt;
-            Vector2 prev_pt;
-            float speed = 0.1f;
-            GameObject hook_obj;
-            Rigidbody2D hook_rb;
-            Information info;
-            bool made_contact = false;
-
-            public Hook(Vector2 start_pt, Vector2 direction, Information info) {
-                this.info = info;
-                hook_obj = Instantiate(info.hook_prefab, start_pt, Quaternion.identity);
-                hook_rb = hook_obj.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
-                curr_pt = start_pt;
-                prev_pt = start_pt - (direction * speed);
+            if (hook_hit) {
+                constraints();
             }
+        }
 
-            public void move(){
-                if (!made_contact) {
-                    Vector2 diff = curr_pt - prev_pt;
-                    prev_pt = curr_pt;
-                    curr_pt += diff;
-                    int mask = LayerMask.GetMask("Collision");
-                    RaycastHit2D hit = Physics2D.Raycast(prev_pt, diff, diff.magnitude, mask);
-                    if (hit.collider != null){
-                        curr_pt = hit.point;
-                        made_contact = true;
-                    }
-                    // curr_pt += info.gravity;
-                    hook_rb.MovePosition(curr_pt);
+        public void create_rope() {
+            if (!hook_hit){
+                Vector2 hook_pos = ball_rope_hook_rb[ball_rope_hook_rb.Count - 1].position;
+                Vector2 newest_rope_pos = ball_rope_hook_rb[ball_rope_hook_rb.Count - 2].position;
+                Vector2 diff = hook_pos - newest_rope_pos;
+                while (diff.magnitude > info.rope_length){
+                    newest_rope_pos = newest_rope_pos + (diff.normalized * info.rope_length);
+                    info.rope_parts_obj[info.active_rope_segments].transform.localPosition = newest_rope_pos;
+                    info.rope_parts_obj[info.active_rope_segments].SetActive(true); 
+                    ball_rope_hook_rb.Insert(ball_rope_hook_rb.Count - 1, info.rope_parts_rb[info.active_rope_segments]);
+
+                    info.active_rope_segments += 1;
+                    diff = hook_pos - newest_rope_pos;
+                    
                 }
             }
         }
-    }
 
-    public class Information {
-        public Vector2 start_pt;
-        public GameObject player_prefab;
-        public GameObject rope_prefab;
-        public GameObject hook_prefab;
-        public Vector2 gravity = new Vector2(0, -0.001f);
-        public float rope_dist;
+        public void constraints() {
 
-        public List<Transform> rope_joints_trans = new List<Transform>();
-        public List<Rigidbody2D> rope_joints_rb = new List<Rigidbody2D>();
-        public List<GameObject> rope_joints_obj = new List<GameObject>();
-
-        public Information(Vector2 loc, GameObject pp, GameObject rp, GameObject hp){
-            this.start_pt = loc;
-            this.player_prefab = pp;
-            this.rope_prefab = rp;
-            this.hook_prefab = hp;
-            rope_dist = 0.2f;
-
-            create_rope_joints(100);
         }
 
-        public void create_rope_joints(int num_to_make){
-            for (int i = 0; i < num_to_make; i++){
-                GameObject joint = Instantiate(rope_prefab, Vector2.zero, Quaternion.identity);
-                joint.SetActive(false);
-                rope_joints_trans.Add(joint.transform);
-                rope_joints_rb.Add(joint.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D);
-                rope_joints_obj.Add(joint);
+        public void ball_add_force(Vector2 force){
+            if (ball_rope_hook_prev_pos.Count > 0){
+                ball_rope_hook_prev_pos[0] -= force;
+            } 
+        }
+
+        public void shoot_hook(Vector2 direction){
+            hook_obj = Instantiate(info.hook_prefab, ball_rope_hook_rb[0].position, Quaternion.identity);
+            Rigidbody2D hook_rb = hook_obj.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+            ball_rope_hook_prev_pos.Add(hook_rb.position - (direction * shoot_speed));
+            ball_rope_hook_rb.Add(hook_rb);
+            active_hook = true;
+            shoot_drection = direction.normalized;
+        }
+    }
+
+    
+
+    public class Information {
+        public GameObject ball_prefab;
+        public GameObject rope_prefab;
+        public GameObject hook_prefab;
+        public Vector2 start_loc;
+        public List<GameObject> rope_parts_obj = new List<GameObject>();
+        public List<Rigidbody2D> rope_parts_rb = new List<Rigidbody2D>();
+        public Vector2 gravity = new Vector2(0f, -0.001f);
+        public float rope_length = 0.3f;
+
+        public int active_rope_segments = 0;
+
+        public Information(GameObject b, GameObject r, GameObject h, Vector2 start_loc){
+            ball_prefab = b;
+            rope_prefab = r;
+            hook_prefab = h;
+            this.start_loc = start_loc;
+            generate_rope();
+        }
+
+        public void generate_rope(){
+            for (int i = 0; i < 100; i++){
+                GameObject rope_part = Instantiate(rope_prefab, Vector2.zero, Quaternion.identity);
+                rope_part.SetActive(false);
+                Rigidbody2D rb = rope_part.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+                rope_parts_obj.Add(rope_part);
+                rope_parts_rb.Add(rb);
             }
         }
     }
